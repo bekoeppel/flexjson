68278ec9200f12473a2b492ddf73773f4806ffee
charliehubbard <charliehubbard@1649a90f-f54c-0410-9814-d0bc849eed40>
2011-06-25 18:20:06 +0000

    Documentation fixes for 2.x.
    git-svn-id: svn://svn.code.sf.net/p/flexjson/code/trunk@138 1649a90f-f54c-0410-9814-d0bc849eed40

--- flexjson2/src/main/java/flexjson/transformer/Transformer.java
+++ flexjson2/src/main/java/flexjson/transformer/Transformer.java
@@ -19,7 +19,7 @@ package flexjson.transformer;
  * Transformers are used to alter the values written to a Flexjson stream.
  * This allows you to modify your data for use with HTML, security like stripping
  * out &lt;script&gt; tags, or rendering HTML from simple markups like markdown or other
- * technologies.  Use {@link JSONSerializer#transform} to register a Transformer to with
+ * technologies.  Use {@link flexjson.JSONSerializer#transform} to register a Transformer to with
  * a JSONSerializer.
  */
 public interface Transformer {
